abe2b3794939f9eed6e1f3e30fff5b3d875436f7
lequocthinh <lqt542001@gmail.com>
2025-12-16 15:23:54 +0700

    [2025-12-16 15:23] Add new feature listening

---
+++ audio/listeningLists.js
@@ -0,0 +1,55 @@
+const listeningLists = [
+    {
+        title: 'QUYỂN 1',
+        tracks: [
+            {
+                title: 'Bài 1: 01-01',
+                src: 'audio/mp3/01-01.mp3',
+                text: [
+                    // { speaker: '', content: '' },
+                    { speaker: '明華', content: '請問你是陳月美小烜嗎 ？' },
+                    { speaker: '月美', content: '是的 。謝謝你來接我們 。' },
+                    { speaker: '明華', content: '不客氣 。我是李明華 。' },
+                    { speaker: '月美', content: '這是王先生 。' },
+                    { speaker: '開文', content: '你好。我姓王，叫開文 。' },
+                    { speaker: '明華', content: '你們好 。歡迎你們來臺灣 。' },
+                ]
+            },
+            {
+                title: 'Bài 1: 01-03',
+                src: 'audio/mp3/01-03.mp3',
+                text: [
+                    { speaker: '明華', content: '請喝茶。' },
+                    { speaker: '開文', content: '謝謝。很好喝。請問這是什麼茶？' },
+                    { speaker: '明華', content: '這是烏龍茶。臺灣人喜歡喝茶。' },
+                    { speaker: '明華', content: '開文，你們日本人呢？' },
+                    { speaker: '月美', content: '他不是日本人。' },
+                    { speaker: '明華', content: '對不起，你是哪國人？' },
+                    { speaker: '開文', content: '我是美國人。' },
+                    { speaker: '明華', content: '開文，你要不要喝咖啡？' },
+                    { speaker: '開文', content: '謝謝！我不喝咖啡，我喜歡喝茶。' },
+                ]
+            },
+            // {
+            //     title: 'Bài 1: 01-01',
+            //     src: 'audio/mp3/01-01.mp3',
+            //     text: [
+            //         // { speaker: '', content: '' },
+            //     ]
+            // },
+        ]
+    },
+    {
+        title: 'A1 – Mua sắm',
+        tracks: [
+            {
+                title: 'Bài 1: Hỏi giá',
+                src: 'audio/mp3/01-03.mp3',
+                text: [
+                    { speaker: 'A', content: '這個多少錢？' },
+                    { speaker: 'B', content: '一百塊。' }
+                ]
+            }
+        ]
+    },
+];
